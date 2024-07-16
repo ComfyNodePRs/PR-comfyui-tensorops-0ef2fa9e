1cc5fa0ddbef12e2dae31331506333dc1b1a93f5
shkr <shashank.f1@gmail.com>
2024-07-16 17:02:28 +0530

    remove

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,15 +1,16 @@
 from .channel_select import ChannelSelector
 from .mask_image import MaskImage
+from .save_surreal import SaveToSurreal
 
-# A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "ChannelSelector": ChannelSelector,
-    "MaskImage": MaskImage
+    "MaskImage": MaskImage,
+    "SaveToSurreal": SaveToSurreal,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ChannelSelector":"ChannelSelector",
-    "MaskImage": "MaskImage"
+    "MaskImage": "MaskImage",
+    "SaveToSurreal": "SaveToSurreal",
 }
---
+++ nodes/save_surreal.py
@@ -0,0 +1,32 @@
+from surrealist import Surreal, Connection as SurrealConnection
+
+SURREAL_URL = ""
+SURREAL_NAMESPACE = ""
+SURREAL_USER = ""
+SURREAL_PASSWORD = ""
+SURREAL_TABLE = ""
+
+class SaveToSurreal:
+   
+    @classmethod    
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "database": ("STRING", {"multiline": False}),
+                "json": ("JSON",),
+                "id": ("STRING", {"multiline": False})
+            },
+        }
+
+    RETURN_TYPES = ()
+
+    FUNCTION = "main"
+
+    CATEGORY = "database_ops"
+
+    def main(self, database: str, json: str, id: str) -> None:
+        surreal_client = Surreal(SURREAL_URL, namespace=SURREAL_NAMESPACE, database=database, credentials=(SURREAL_USER, SURREAL_PASSWORD), use_http=True, timeout=10)
+        surreal_connection =  surreal_client.connect()
+        query = f"CREATE {SURREAL_TABLE}:`{id}` CONTENT {{'json': {json}}};"
+        surreal_connection.query(query)
+
