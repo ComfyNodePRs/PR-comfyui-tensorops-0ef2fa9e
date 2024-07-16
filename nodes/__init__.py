aeb5ccd3e99e21fc8a3b8ff8a68ed8ad3f6b0c3d
shkr <shashank.f1@gmail.com>
2024-07-16 17:48:05 +0530

    remove

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,16 +1,18 @@
 from .channel_select import ChannelSelector
 from .mask_image import MaskImage
-from .save_surreal import SaveToSurreal
+from .save_surreal import SaveJsonToSurreal, SaveTextToSurreal
 
 NODE_CLASS_MAPPINGS = {
     "ChannelSelector": ChannelSelector,
     "MaskImage": MaskImage,
-    "SaveToSurreal": SaveToSurreal,
+    "SaveJsonToSurreal": SaveJsonToSurreal,
+    "SaveTextToSurreal": SaveTextToSurreal,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ChannelSelector":"ChannelSelector",
     "MaskImage": "MaskImage",
-    "SaveToSurreal": "SaveToSurreal",
+    "SaveJsonToSurreal": "SaveJsonToSurreal",
+    "SaveTextToSurreal": "SaveTextToSurreal",
 }
--- nodes/save_surreal.py
+++ nodes/save_surreal.py
@@ -1,4 +1,4 @@
-from surrealist import Surreal, Connection as SurrealConnection
+from surrealist import Surreal
 
 SURREAL_URL = ""
 SURREAL_NAMESPACE = ""
@@ -6,7 +6,7 @@ SURREAL_USER = ""
 SURREAL_PASSWORD = ""
 SURREAL_TABLE = ""
 
-class SaveToSurreal:
+class SaveJsonToSurreal:
    
     @classmethod    
     def INPUT_TYPES(s):
@@ -14,7 +14,8 @@ class SaveToSurreal:
             "required": {
                 "database": ("STRING", {"multiline": False}),
                 "json": ("JSON",),
-                "id": ("STRING", {"multiline": False})
+                "id": ("STRING", {"multiline": False}),
+                "key": ("STRING", {"multiline": False})
             },
         }
 
@@ -24,9 +25,34 @@ class SaveToSurreal:
     OUTPUT_NODE = True
     CATEGORY = "database_ops"
 
-    def main(self, database: str, json: str, id: str) -> None:
+    def main(self, database: str, id: str, key: str, json: str) -> None:
         surreal_client = Surreal(SURREAL_URL, namespace=SURREAL_NAMESPACE, database=database, credentials=(SURREAL_USER, SURREAL_PASSWORD), use_http=True, timeout=10)
         surreal_connection =  surreal_client.connect()
-        query = f"CREATE {SURREAL_TABLE}:`{id}` CONTENT {{'json': {json}}};"
+        query = f"CREATE {SURREAL_TABLE}:`{id}` CONTENT {{'{key}': {json}}};"
         surreal_connection.query(query)
 
+
+class SaveTextToSurreal:
+   
+    @classmethod    
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "database": ("STRING", {"multiline": False}),
+                "text":  ("STRING", {"multiline": True}),
+                "id": ("STRING", {"multiline": False}),
+                "key": ("STRING", {"multiline": False})
+            },
+        }
+
+    RETURN_TYPES = ()
+
+    FUNCTION = "main"
+    OUTPUT_NODE = True
+    CATEGORY = "database_ops"
+
+    def main(self, database: str, id: str, key: str, text: str) -> None:
+        surreal_client = Surreal(SURREAL_URL, namespace=SURREAL_NAMESPACE, database=database, credentials=(SURREAL_USER, SURREAL_PASSWORD), use_http=True, timeout=10)
+        surreal_connection =  surreal_client.connect()
+        query = f"CREATE {SURREAL_TABLE}:`{id}` CONTENT {{'{key}': '{text}'}};"
+        surreal_connection.query(query)
